d18890620de156cd699a89f4b09eb177077fb6eb
ledarch <david.ortega34000@gmail.com>
2018-08-09 20:19:05 +0200

    first commit create project

---
+++ Maven/Roller-Derby-Manager/src/main/java/fr/sac/RollerDerbyManager/RollerDerbyManagerApplication.java
@@ -0,0 +1,12 @@
+package fr.sac.RollerDerbyManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RollerDerbyManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RollerDerbyManagerApplication.class, args);
+	}
+}
